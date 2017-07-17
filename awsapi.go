2096169c1ff7a26c3f20842684bfd1e508899f73
WedgeNix <Mullen.bryce@gmail.com>
2017-07-17 14:40:56 -0700

    Took out optional dir in PutObject() and created a new func PutObjectDir()

--- awsapi.go
+++ awsapi.go
@@ -7,6 +7,8 @@ import (
 
 	"fmt"
 
+	"strings"
+
 	"github.com/WedgeNix/util"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -43,16 +45,40 @@ func (ac *AwsController) GetObject(key string, inter interface{}) {
 	json.NewDecoder(result.Body).Decode(&inter)
 }
 
-// PutObject sends a file to AWS S3 bucket, uses name of file on system, and AWS directory if there is one
-func (ac *AwsController) PutObject(name string, dir ...string) *s3.PutObjectOutput {
+// PutObject sends a file to AWS S3 bucket, uses name of file.
+// This will Put the file in the main bucket directory.
+func (ac *AwsController) PutObject(name string) *s3.PutObjectOutput {
 	f, err := os.Open(name)
-	var key string
-	if len(dir) > 0 {
-		key = dir[0] + "/" + name
-	} else {
-		key = name
+	key := splitName(name)
+	imput := &s3.PutObjectInput{
+		Body:                 aws.ReadSeekCloser(f),
+		Bucket:               aws.String(ac.bucket),
+		Key:                  aws.String(key),
+		ServerSideEncryption: aws.String("AES256"),
 	}
-	util.E(err)
+	result, err := ac.c3svc.PutObject(imput)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return nil
+	}
+	return result
+}
+
+// PutObjectDir sends a file to AWS S3 bucket, uses name of file and directory to save file.
+// This will Put the file in the directory specified.
+func (ac *AwsController) PutObjectDir(name string, dir string) *s3.PutObjectOutput {
+	f, err := os.Open(name)
+	keyName := splitName(name)
+	key := dir + "/" + keyName
 	imput := &s3.PutObjectInput{
 		Body:                 aws.ReadSeekCloser(f),
 		Bucket:               aws.String(ac.bucket),
@@ -99,4 +125,9 @@ func (ac *AwsController) GetList() *s3.ListObjectsOutput {
 		return nil
 	}
 	return result
+}
+
+func splitName(name string) string {
+	sp := strings.Split(name, "/")
+	return sp[len(sp)-1]
 }
