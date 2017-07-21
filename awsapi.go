63460119371d8438519cae5e19c8ae9a0a2fb5ed
PǝRV <alexander.w.matulionis@gmail.com>
2017-07-21 13:03:46 -0700

    Alex's bastardizing

--- awsapi.go
+++ awsapi.go
@@ -10,11 +10,14 @@ import (
 
 	"strings"
 
+	"bytes"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/mrmiguu/un"
 )
 
 // AwsController method struct for StartAWS
@@ -53,15 +56,18 @@ type ObjectMonitors map[string]Monitor
 // allows ObjectMonitors to be bound to the Object interface
 func (_ ObjectMonitors) __() {}
 
-// GetObject gets JSON from AWS S3 populates the custom struct of the file
+// Get gets JSON from AWS S3 populates the custom struct of the file
 // key is the dir + "/" + filename
 // returns false if err reads NoSuchKey meaning does not exist. Can read true
 // if another error happens, so must determain how to handle error
-func (ac *AwsController) GetObject(key string, o Object) (bool, error) {
-	result, err := ac.c3svc.GetObject(&s3.GetObjectInput{
+func (ac *AwsController) Get(key string, o Object) (bool, error) {
+	input := &s3.GetObjectInput{
 		Bucket: aws.String(ac.bucket),
 		Key:    aws.String(key),
-	})
+	}
+
+	result, err := ac.c3svc.GetObject(input)
+
 	if err == nil {
 		json.NewDecoder(result.Body).Decode(o)
 		return true, nil
@@ -70,61 +76,21 @@ func (ac *AwsController) GetObject(key string, o Object) (bool, error) {
 	return !strings.Contains(err.Error(), "NoSuchKey"), err
 }
 
-// PutObject sends a file to AWS S3 bucket, uses name of file.
+// Put sends a file to AWS S3 bucket, uses name of file.
 // This will Put the file in the main bucket directory.
-func (ac *AwsController) PutObject(name string) *s3.PutObjectOutput {
-	f, err := os.Open(name)
-	key := splitName(name)
-	imput := &s3.PutObjectInput{
-		Body:                 aws.ReadSeekCloser(f),
-		Bucket:               aws.String(ac.bucket),
-		Key:                  aws.String(key),
-		ServerSideEncryption: aws.String("AES256"),
-	}
-	result, err := ac.c3svc.PutObject(imput)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return nil
-	}
-	return result
-}
+func (ac *AwsController) Put(filename string, o Object) (string, error) {
+	r := bytes.NewReader(un.Bytes(json.Marshal(o)))
 
-// PutObjectDir sends a file to AWS S3 bucket, uses name of file and directory to save file.
-// This will Put the file in the directory specified.
-func (ac *AwsController) PutObjectDir(name string, dir string) *s3.PutObjectOutput {
-	f, err := os.Open(name)
-	keyName := splitName(name)
-	key := dir + "/" + keyName
-	imput := &s3.PutObjectInput{
-		Body:                 aws.ReadSeekCloser(f),
+	input := &s3.PutObjectInput{
+		Body:                 aws.ReadSeekCloser(r),
 		Bucket:               aws.String(ac.bucket),
-		Key:                  aws.String(key),
+		Key:                  aws.String(filename),
 		ServerSideEncryption: aws.String("AES256"),
 	}
-	result, err := ac.c3svc.PutObject(imput)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return nil
-	}
-	return result
+
+	result, err := ac.c3svc.PutObject(input)
+
+	return *result.VersionId, err
 }
 
 // GetList gets a list of files in the bucket
@@ -150,9 +116,4 @@ func (ac *AwsController) GetList() *s3.ListObjectsOutput {
 		return nil
 	}
 	return result
-}
-
-func splitName(name string) string {
-	sp := strings.Split(name, "/")
-	return sp[len(sp)-1]
 }
